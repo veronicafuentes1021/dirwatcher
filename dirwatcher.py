a255343ca2cd051df66343359de2da6032ee041c
Veronica Fuentes <veronica.elsa1021@gmail.com>
2020-10-24 04:23:49 -0500

    creating a parser

--- dirwatcher.py
+++ dirwatcher.py
@@ -6,6 +6,10 @@ Dirwatcher - A long-running program
 __author__ = 'Veronica Fuentes'
 
 import sys
+from datetime import time
+import os
+import logging
+import argparse
 
 
 def search_for_magic(filename, start_line, magic_string):
@@ -14,13 +18,46 @@ def search_for_magic(filename, start_line, magic_string):
 
 
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
-    return
+    file_holder = {}
+    exit = False
+    while not exit:
+        time.sleep(interval)
+        if os.path.isdir(path):
+            file_list = os.listdir(path)
+            for files in file_list:
+                if files.endswith(extension) and files not in file_holder:
+                    file_holder[files] = 0
+                    logging.info(f'file added {files}')
+            key = list(file_holder.keys())
+            for k in key:
+                if k not in file_list:
+                    print(f'This file has been removed {k}')
+                    file_holder.pop(k)
+            for key, value in file_holder.items():
+                file_holder[key] = search_for_magic(
+                    f'{path}/{key}', value, magic_string)
+            print(file_holder)
+        else:
+            logging.error(f'{path} directory not found.')
+            file_holder = {}
 
 
 def create_parser():
-    # Your code here
-    return
+    parser = argparse.ArgumentParser(
+        description='Watches specified directory for a specific input.')
+    parser.add_argument(
+        'directory',
+        help='directory to watch')
+    parser.add_argument(
+        'input',
+        help='input to scan directory for')
+    parser.add_arguemnt(
+        '-e', '--extension',
+        help='file extention to search', default='.txt')
+    parser.add_arguemnt(
+        '-i', '--interval',
+        help='poll int, defaults to 1 sec', type=float, default=1.0)
+    return parser
 
 
 def signal_handler(sig_num, frame):
